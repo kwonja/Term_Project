4569f989862674a763a85f80e884bed811e66c86
kwonja <kownja87@gmail.com>
2022-12-04 02:51:03 +0900

    Locker connect

---
+++ backend/ViewLocker.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+
+$conn = mysqli_connect("192.168.100.3","kwonsungmin","1234","term_project",4567);
+$sql = "SELECT * FROM Locker";
+$result = mysqli_query($conn, $sql);
+// var_dump($result->num_rows);
+$myArray = array();
+// echo "<h1>single row</h1>";
+// $row = mysqli_fetch_array($result);
+// echo '<h1>'.$row['bookid'].'</h1>'
+// echo $row['bookname'];
+// echo '<br/>';
+while($row = $result->fetch_assoc()) {
+    $myArray[] = $row;
+}
+echo json_encode($myArray);
+
+//php 결과 디버깅
+if($result === false){
+    echo mysqli_error($conn);
+}
+?>
